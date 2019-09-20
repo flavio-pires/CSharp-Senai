fe915424b5d6c1730731b2e27b235a47e2a85cca
Flavio Pires <fdnpires@gmail.com>
2019-09-20 11:21:02 -0300

    atualizado 20_09

---
+++ tabuada_5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace tabuada_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Tabuada do 5");
+            for (int tab = 5; tab <= 50; tab+=5){
+                Console.WriteLine(tab);
+            }
+        }
+    }
+}
