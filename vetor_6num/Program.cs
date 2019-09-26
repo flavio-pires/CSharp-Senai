ee7ddb3484bc5214b9a3027aa820dedd53ee1717
Flavio Pires <fdnpires@gmail.com>
2019-09-26 11:26:15 -0300

    aula 26_09

--- vetor_6num/Program.cs
+++ vetor_6num/Program.cs
@@ -7,19 +7,36 @@ namespace vetor_6num
         static void Main(string[] args)
         {
             int [] vetor = new int[6];
-            int num = 0;
+            int cont = 0;
+            int pares = 0;
+            int impares = 0;
 
-            for (num = 0; num < 6; num++){
+            for (cont = 0; cont < 6; cont++){
                           
-            Console.WriteLine("Digite um número inteiro");
-            vetor[num] = int.Parse(Console.ReadLine());
+            Console.Write("Digite um número: ");
+            vetor[cont] = int.Parse(Console.ReadLine());
             }
+            // Estrutura de laço para ler todos os números e identificar quais são pares e ímpares
 
-            if (num % 2 == 0) {
-				Console.WriteLine("Este número é par!");
-			} else {
-                Console.WriteLine("Este número é ímpar!");
+            foreach(int num in vetor){
+                if (num % 2 == 0) {
+				pares += 1;
+                } else {
+                impares ++;
             }
+            }
+            //int num;
+            //for(int cont = 0; cont <= 5; cont++){
+                //num = vetor[cont];
+                //if(num % 2 == 0){
+                    //pares++;
+                //} else {
+                    //impares++;
+                //}
+            //}
+
+            Console.WriteLine($"Você tem {pares} números pares e {impares} números impares");         
+			
             
 
         }    
