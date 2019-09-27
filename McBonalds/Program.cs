dd4dac24cfeb02e813510c7cf48a2045ac196c22
Flavio Pires <fdnpires@gmail.com>
2019-09-27 11:21:03 -0300

    aulaPOO_27_09

---
+++ McBonalds/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace McBonalds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            cliente cliente1 = new cliente("Alexandre","55 11 5555-5555","alexandre@email.com");
+
+            Console.WriteLine("Nome: " + cliente1.Nome);
+            Console.WriteLine("Telefone: " + cliente1.Telefone);
+            Console.WriteLine("Email: " + cliente1.Email);
+
+        }
+    }
+}
